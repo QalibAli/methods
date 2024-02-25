e193d13d2ffa89a6da38dff00b80292473d865e8
Qalib Alizade <qalibalizade@Qalibs-MacBook-Air.local>
2024-02-25 22:44:45 +0400

    second commit

---
+++ new_task.dart
@@ -0,0 +1,8 @@
+// Abituriyent imtahandan cixir ve neticesini yoxlamaq isteyir
+// o 5 fennden imtahan verir. Fizika , Riyaziyyat , Azerbaycan dili, Ingilis dili , Kimya
+// Her fenn 25 sual teshkil edir.
+// Riyaziyyat , Fizika fennlerinin 1 suali 8 bal . Diger fennlerin duzgun cavabi ise 4 bal teshkil edir
+// Istifadeci her fennden duzgun ve yanlis cavablarini inputa daxil etsin
+// 4 sehv sual 1 duz cavabi silir. 1 sehv cavab 1/4 duz cavabin balini silir
+// Sonda istifadecinin daxil etdiyi duzgun ve sehv cavablara esasen imtahan neticesini ekrana cixardin. 
+// Qeyd: aksimum bal ise 700
