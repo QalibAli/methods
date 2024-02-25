2af07a251c3d84aa5611928524b0d8f95b27b4f4
Qalib Alizade <qalibalizade@Qalibs-MacBook-Air.local>
2024-02-26 00:34:16 +0400

    third commit

--- main.dart
+++ main.dart
@@ -1,14 +1,3 @@
-// explain three type methods
-// write simple method - explain that method
-// get type method
-// sum()
-// apply
-// minus
-// division
-// do all operations in other files - show import
-// write calculator with methods
-// write login register
-
 // void get name => 'qalib';
 
 // dynamic password() {
